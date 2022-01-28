ca17273aea21c1ff5b2e909f50e5b342e3f2e4de
Hieu <hieu@render.com>
2022-01-27 16:30:58 -0800

    Trim entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -eu -o pipefail
 
+echo "Render entrypoint.."
 export BIND_ON_IP="${BIND_ON_IP:-$(hostname -i)}"
 if [[ -z "${TEMPORAL_CLI_ADDRESS:-}" ]]; then
   if [[ "${BIND_ON_IP}" =~ ":" ]]; then
@@ -12,17 +13,9 @@ if [[ -z "${TEMPORAL_CLI_ADDRESS:-}" ]]; then
       export TEMPORAL_CLI_ADDRESS="${BIND_ON_IP}:7233"
   fi
 fi
+echo "TEMPORAL_CLI_ADDRESS=${TEMPORAL_CLI_ADDRESS}"
 
 
 dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/config/docker.yaml
-
-# Automatically setup Temporal Server (databases, Elasticsearch, default namespace) if "autosetup" is passed as an argument.
-for arg in "$@" ; do [[ ${arg} == "autosetup" ]] && /etc/temporal/auto-setup.sh && break ; done
-
-# Setup Temporal Server in development mode if "develop" is passed as an argument.
-for arg in "$@" ; do [[ ${arg} == "develop" ]] && /etc/temporal/setup-develop.sh && break ; done
-
-# Run bash instead of Temporal Server if "bash" is passed as an argument (convenient to debug docker image).
-for arg in "$@" ; do [[ ${arg} == "bash" ]] && bash && exit 0 ; done
 
 exec /etc/temporal/start-temporal.sh
